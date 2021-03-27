0316e60d44e70dfb02ab5ff7aaa0b24840d1d320
Sivecano <Sivecano@gmail.com>
2021-03-27 21:19:06 +0100

    better errors

--- common.cpp
+++ common.cpp
@@ -2,18 +2,24 @@
 // Created by sivecano on 27/03/2021.
 //
 
+#include "SDL2/SDL_net.h"
 #include <exception>
 #include <stdexcept>
 
 template<typename type>
 void recieve(TCPsocket sock, type* data, int bytes)
 {
-    if (SDLNet_TCP_Recv(sock, (void*)data, bytes) <= 0)
-        throw std::runtime_error("we lost connection or something went horribly wrong\n");
+    if (SDLNet_TCP_Recv(sock, (void*)data, bytes) <= 0){
+        std::cout << "Error "<< SDLNet_GetError() << "\n";
+        throw std::runtime_error("error in reviece\n");
+    }
+
 }
 template<typename type>
 void send(TCPsocket sock, type* data, int bytes)
 {
-    if (SDLNet_TCP_Send(sock, (void*)data, bytes) < bytes)
-        throw std::runtime_error("we lost connection or something went horribly wrong\n");
+    if (SDLNet_TCP_Send(sock, (void*)data, bytes) < bytes) {
+        std::cout << "Error "<< SDLNet_GetError() << "\n";
+        throw std::runtime_error("error in send\n");
+    }
 }
